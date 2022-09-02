9a6f864c3ff1b45704cdf48141c59655f1780312
Marco <marco.silva631@outlook.com>
2022-09-01 21:20:52 -0300

    Adjusting horizontal scrolling, adding links to social networks and removing unnecessary paddings

--- js/script.js
+++ js/script.js
@@ -4,3 +4,8 @@ const navLinks = document.querySelector("#navLinks");
 toggleBtn.addEventListener("click", () => {
   navLinks.classList.toggle("d-none");
 });
+
+
+function Agendar(){
+  window.open("https://wa.me/?text=Gostaria%20de%20marcar%20uma%20avaliação",'_blank');
+}
