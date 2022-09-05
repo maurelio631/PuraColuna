29cced62374405f8f03d60293430abce58946d3d
Marco <marco.silva631@outlook.com>
2022-09-05 19:44:14 -0300

    Changing navbar component

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-const toggleBtn = document.querySelector("#toggleBtn");
-const navLinks = document.querySelector("#navLinks");
-
-toggleBtn.addEventListener("click", () => {
-  navLinks.classList.toggle("d-none");
-});
-
-
 function Agendar(){
   window.open("https://wa.me/?text=Gostaria%20de%20marcar%20uma%20avaliação",'_blank');
 }
