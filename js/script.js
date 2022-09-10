87a8360b273fbed4445c301190a80fcb809cc5db
Marco <marco.silva631@outlook.com>
2022-09-09 22:18:01 -0300

    Adding go to top button and changing section titles from images to h5 tags

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+let mybutton = document.getElementById("btn-back-to-top");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 function Agendar(){
   window.open("https://wa.me/?text=Gostaria%20de%20marcar%20uma%20avaliação",'_blank');
 }
