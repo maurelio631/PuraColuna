46ab20b34f7806b95823bd26e6fafefc3a0dffb5
maurelio631 <marco.silva631@outlook.com>
2023-01-09 23:08:10 -0300

    Mais ajustes

--- js/script.js
+++ js/script.js
@@ -11,7 +11,6 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-// When the user clicks on the button, scroll to the top of the document
 mybutton.addEventListener("click", backToTop);
 
 function backToTop() {
